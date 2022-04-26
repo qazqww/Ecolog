0578585698dd03a7db489caf6f30890a749b44ef
지수연 <zzzxxzxz@naver.com>
2022-04-26 09:52:50 +0900

    feat : application.yml 분리 및 db 연결

--- backend/src/main/java/com/thedebuggers/backend/domain/repository/TestEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thedebuggers.backend.domain.repository;
-
-import com.thedebuggers.backend.domain.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-}
