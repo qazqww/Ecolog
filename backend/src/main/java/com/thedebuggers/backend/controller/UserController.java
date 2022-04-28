edccd829d9ccaff95d967a5bdc534e18b612cac3
지수연 <zzzxxzxz@naver.com>
2022-04-29 00:12:40 +0900

    feat : 회원 정보 조회 API 수정 - 내 정보 보기 / 타 회원 정보 보기 분리

--- backend/src/main/java/com/thedebuggers/backend/controller/UserController.java
+++ backend/src/main/java/com/thedebuggers/backend/controller/UserController.java
@@ -2,7 +2,8 @@ package com.thedebuggers.backend.controller;
 
 import com.thedebuggers.backend.auth.ELUserDetails;
 import com.thedebuggers.backend.domain.entity.User;
-import com.thedebuggers.backend.dto.UserInfoResDto;
+import com.thedebuggers.backend.dto.MyInfoResDto;
+import com.thedebuggers.backend.dto.ProfileResDto;
 import com.thedebuggers.backend.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -14,10 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Api(value = "사용자 정보 관련 API", tags = {"User"})
 @Slf4j
 @RequestMapping("/api/v1/user")
@@ -27,28 +24,34 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping({"", "/", "/{userNo}"})
-    @ApiOperation(value = "회원 정보 ")
+    @GetMapping
+    @ApiOperation(value = "내 정보")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Server Error")
     })
-    private ResponseEntity<UserInfoResDto> info(@PathVariable(required = false) Long userNo, Authentication authentication) {
+    private ResponseEntity<MyInfoResDto> myInfo(Authentication authentication) {
 
-        User user = null;
-
-        if (userNo == null) {
             ELUserDetails userDetails = (ELUserDetails) authentication.getDetails();
-            user = userDetails.getUser();
-        } else {
-            user = userService.getUserByUserNo(userNo);
-        }
+            User user = userDetails.getUser();
 
-
-        return ResponseEntity.ok(user != null ? UserInfoResDto.of(user) : null);
+        return ResponseEntity.ok(MyInfoResDto.of(user));
     }
 
+    @GetMapping("/{userNo}")
+    @ApiOperation(value = "프로필 조회")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+    private ResponseEntity<ProfileResDto> profile(@PathVariable Long userNo) {
+
+        User user = userService.getUserByUserNo(userNo);
+
+        return ResponseEntity.ok(user != null ? ProfileResDto.of(user) : null);
+    }
     @DeleteMapping
     @ApiOperation(value = "회원 탈퇴")
     @ApiResponses({
