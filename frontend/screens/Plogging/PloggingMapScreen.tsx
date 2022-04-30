29eb8fe7d76ac36e46856e81d07c342452fad0fc
Hui-Story <leejaehee0728@gmail.com>
2022-04-30 23:38:35 +0900

    feat: 플로깅 종료 후 인증사진 촬영 구현
    react-native-image-picker 라이브러리 설치
    플로깅 종료 버튼을 누르면 인증사진 촬영으로 이동
    Image uri 출력되는 것을 확인함

--- frontend/screens/Plogging/PloggingMapScreen.tsx
+++ frontend/screens/Plogging/PloggingMapScreen.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import {Text, View, TouchableHighlight} from 'react-native';
+import {Text, View, Platform, TouchableHighlight} from 'react-native';
+import {
+  launchCamera,
+  CameraOptions,
+  ImagePickerResponse,
+} from 'react-native-image-picker';
+
+const imagePickerOption: CameraOptions = {
+  mediaType: 'photo',
+  maxWidth: 768,
+  maxHeight: 768,
+  // includeBase64: Platform.OS === 'android',
+};
 
 function PloggingMapScreen({navigation}: any) {
+  const onPickImage = (res: ImagePickerResponse) => {
+    if (res.didCancel || !res) {
+      return;
+    }
+    console.log(res);
+    navigation.navigate('PloggingResult', {res});
+  };
+
+  const onLaunchCamera = () => {
+    launchCamera(imagePickerOption, onPickImage);
+  };
+
   return (
     <View>
       <Text>This is PloggingMap</Text>
-      <TouchableHighlight
-        onPress={() => {
-          navigation.navigate('PloggingResult');
-        }}
-        underlayColor="red">
+      <TouchableHighlight onPress={() => onLaunchCamera()} underlayColor="red">
         <Text>Go to Result</Text>
       </TouchableHighlight>
     </View>
