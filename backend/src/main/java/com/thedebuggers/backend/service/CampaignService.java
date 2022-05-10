b4330afbbc2c92dbc842d91b10540fd42f3cffa1
p9a3r5k0 <p9a3r5k0@naver.com>
2022-05-10 13:21:19 +0900

    refact : 캠페인 모집 API 로직 수정

--- backend/src/main/java/com/thedebuggers/backend/service/CampaignService.java
+++ backend/src/main/java/com/thedebuggers/backend/service/CampaignService.java
@@ -3,22 +3,23 @@ package com.thedebuggers.backend.service;
 import com.thedebuggers.backend.domain.entity.Campaign;
 import com.thedebuggers.backend.domain.entity.User;
 import com.thedebuggers.backend.dto.CampaignReqDto;
+import com.thedebuggers.backend.dto.CampaignResDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface CampaignService {
-    Campaign registCampaign(CampaignReqDto campaignReqDto, long communityNo, User user, MultipartFile imageFile);
+    CampaignResDto registCampaign(CampaignReqDto campaignReqDto, long communityNo, User user, MultipartFile imageFile);
 
-    List<Campaign> getCampaignList(long communityNo);
+    List<CampaignResDto> getCampaignList(long communityNo);
 
     List<User> getCampaignMember(long campaignNo);
 
-    Campaign getCampaign(long campaignNo);
+    CampaignResDto getCampaign(long campaignNo);
 
-    void joinCampaign(Campaign campaign, User user);
+    CampaignResDto joinCampaign(long campaignNo, User user);
 
-    Campaign updateCampaign(CampaignReqDto campaignReqDto, long campaignNo, User user, MultipartFile imageFile);
+    CampaignResDto updateCampaign(CampaignReqDto campaignReqDto, long campaignNo, User user, MultipartFile imageFile);
 
     boolean deleteCampaign(User user, long campaignNo);
 }
