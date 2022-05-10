fba55fcfbd16ef822261fcc900179938ca9a22c4
ek_13 <kldoggod@gmail.com>
2022-05-10 13:23:13 +0900

    feat : 유니티 아바타 프로젝트 추가
    Former-commit-id: 7bdb9b21324b8fc7168e9b9766de94552badba6c

---
+++ frontend/unity/builds/android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,153 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void NativeAPI::.ctor()
+extern void NativeAPI__ctor_m18E74AFFE46A30E49D4DFDD14BB1D30EEB609295 (void);
+// 0x00000002 System.Void ButtonBehavior::ButtonPressed()
+extern void ButtonBehavior_ButtonPressed_m97FB4C02ADC9F6F5810ABC598025B69C1ED319AE (void);
+// 0x00000003 System.Void ButtonBehavior::.ctor()
+extern void ButtonBehavior__ctor_mD5FCC1CD720C7A699588B2678E76843B3F0274EB (void);
+// 0x00000004 System.Void CharacterSelection::Start()
+extern void CharacterSelection_Start_m0D36A57979683BEA4A3C22CAD9B5ED0063D3761D (void);
+// 0x00000005 System.Void CharacterSelection::ToggleLeft()
+extern void CharacterSelection_ToggleLeft_mFFC917E78203FE10905A8A0BE642643504F70754 (void);
+// 0x00000006 System.Void CharacterSelection::ToggleRight()
+extern void CharacterSelection_ToggleRight_m3DF782B7A0C2AC60CC3389A66FEFA0D8FE5FA3F4 (void);
+// 0x00000007 System.Void CharacterSelection::CofrimButton()
+extern void CharacterSelection_CofrimButton_m3B41358B44C6F98D002D774B69F654847A6221DC (void);
+// 0x00000008 System.Void CharacterSelection::RoomLeft()
+extern void CharacterSelection_RoomLeft_mD6562EF3ADF42799318C42B66C69C42DB74E9534 (void);
+// 0x00000009 System.Void CharacterSelection::RoomRight()
+extern void CharacterSelection_RoomRight_m89E18307EC217AF10005960B94F585779042D60A (void);
+// 0x0000000A System.Void CharacterSelection::RoomConfirm()
+extern void CharacterSelection_RoomConfirm_mDC7BC1276D3A1C0FB9E3D0BA3F76EE59C2341B9F (void);
+// 0x0000000B System.Void CharacterSelection::ChangeScene()
+extern void CharacterSelection_ChangeScene_mD7C91AA54BE0449A464C6159646BFDEA6C22FD9C (void);
+// 0x0000000C System.Void CharacterSelection::ButtonPressed(System.String)
+extern void CharacterSelection_ButtonPressed_mF5FBB59E4A2942B0932CF180BF41F7603BCDF24A (void);
+// 0x0000000D System.Void CharacterSelection::MessageRN(System.String)
+extern void CharacterSelection_MessageRN_m98E1FD2216063D95B5AF4D477CBE70A7399414F1 (void);
+// 0x0000000E System.Void CharacterSelection::SetCoinText()
+extern void CharacterSelection_SetCoinText_m19AC506926C510ADB978262C30311032ECF62A16 (void);
+// 0x0000000F System.Void CharacterSelection::SetAvatar()
+extern void CharacterSelection_SetAvatar_m66A9F735BCC47CDCE2FCA10E42E1327E5DB7E067 (void);
+// 0x00000010 System.Void CharacterSelection::SetRoom()
+extern void CharacterSelection_SetRoom_m4196E35643CD772AA1A927C62DF1FDBA2A21D85E (void);
+// 0x00000011 System.Void CharacterSelection::.ctor()
+extern void CharacterSelection__ctor_m3E62C8F7B93CE79CAE062EE9EBB4E174DD97E5DC (void);
+// 0x00000012 System.Void MoveSelect::Start()
+extern void MoveSelect_Start_m0F6CDBE4BADF4B02A7CEC42DE86E5FA3A2543B28 (void);
+// 0x00000013 System.Void MoveSelect::Update()
+extern void MoveSelect_Update_mC24B8135F95F99D5195DB7E0EFBDCD1059E3FCB4 (void);
+// 0x00000014 System.Void MoveSelect::MoveSelectButton()
+extern void MoveSelect_MoveSelectButton_m8A8339C07F850639559F45E1DA2D34F1F835E51D (void);
+// 0x00000015 System.Void MoveSelect::MoveRoomSelectButton()
+extern void MoveSelect_MoveRoomSelectButton_mC050B1167A702194EF4B9E2BDA538FBF2AEB1D9C (void);
+// 0x00000016 System.Void MoveSelect::.ctor()
+extern void MoveSelect__ctor_m1F1B9CD91DCF89B86CF7B774ED75FFC05ABB4A37 (void);
+// 0x00000017 System.Void RNScript::MessageRN(System.String)
+extern void RNScript_MessageRN_mA028B7B52DA9EC72838AFD4B31F40AC56019B4D6 (void);
+// 0x00000018 System.Void RNScript::SetCoinText()
+extern void RNScript_SetCoinText_mB09B672BB919B94BF53F14601D86B18B9FBCFDD9 (void);
+// 0x00000019 System.Void RNScript::.ctor()
+extern void RNScript__ctor_m8FB6840804EC85A52C7A00554BFFE6FBB7BAE39D (void);
+// 0x0000001A System.Void UnityDebug::Awake()
+extern void UnityDebug_Awake_mE0AE29D996332EFAC4CF2C946488844FC417D6A6 (void);
+// 0x0000001B System.Void UnityDebug::OnApplicationPause(System.Boolean)
+extern void UnityDebug_OnApplicationPause_m28F0154E2072830E24687E1CFF27584F2E91B32E (void);
+// 0x0000001C System.Void UnityDebug::.ctor()
+extern void UnityDebug__ctor_m2A42FAD94D11E0896E165AE3D8B0BC7B61AE77F7 (void);
+static Il2CppMethodPointer s_methodPointers[28] = 
+{
+	NativeAPI__ctor_m18E74AFFE46A30E49D4DFDD14BB1D30EEB609295,
+	ButtonBehavior_ButtonPressed_m97FB4C02ADC9F6F5810ABC598025B69C1ED319AE,
+	ButtonBehavior__ctor_mD5FCC1CD720C7A699588B2678E76843B3F0274EB,
+	CharacterSelection_Start_m0D36A57979683BEA4A3C22CAD9B5ED0063D3761D,
+	CharacterSelection_ToggleLeft_mFFC917E78203FE10905A8A0BE642643504F70754,
+	CharacterSelection_ToggleRight_m3DF782B7A0C2AC60CC3389A66FEFA0D8FE5FA3F4,
+	CharacterSelection_CofrimButton_m3B41358B44C6F98D002D774B69F654847A6221DC,
+	CharacterSelection_RoomLeft_mD6562EF3ADF42799318C42B66C69C42DB74E9534,
+	CharacterSelection_RoomRight_m89E18307EC217AF10005960B94F585779042D60A,
+	CharacterSelection_RoomConfirm_mDC7BC1276D3A1C0FB9E3D0BA3F76EE59C2341B9F,
+	CharacterSelection_ChangeScene_mD7C91AA54BE0449A464C6159646BFDEA6C22FD9C,
+	CharacterSelection_ButtonPressed_mF5FBB59E4A2942B0932CF180BF41F7603BCDF24A,
+	CharacterSelection_MessageRN_m98E1FD2216063D95B5AF4D477CBE70A7399414F1,
+	CharacterSelection_SetCoinText_m19AC506926C510ADB978262C30311032ECF62A16,
+	CharacterSelection_SetAvatar_m66A9F735BCC47CDCE2FCA10E42E1327E5DB7E067,
+	CharacterSelection_SetRoom_m4196E35643CD772AA1A927C62DF1FDBA2A21D85E,
+	CharacterSelection__ctor_m3E62C8F7B93CE79CAE062EE9EBB4E174DD97E5DC,
+	MoveSelect_Start_m0F6CDBE4BADF4B02A7CEC42DE86E5FA3A2543B28,
+	MoveSelect_Update_mC24B8135F95F99D5195DB7E0EFBDCD1059E3FCB4,
+	MoveSelect_MoveSelectButton_m8A8339C07F850639559F45E1DA2D34F1F835E51D,
+	MoveSelect_MoveRoomSelectButton_mC050B1167A702194EF4B9E2BDA538FBF2AEB1D9C,
+	MoveSelect__ctor_m1F1B9CD91DCF89B86CF7B774ED75FFC05ABB4A37,
+	RNScript_MessageRN_mA028B7B52DA9EC72838AFD4B31F40AC56019B4D6,
+	RNScript_SetCoinText_mB09B672BB919B94BF53F14601D86B18B9FBCFDD9,
+	RNScript__ctor_m8FB6840804EC85A52C7A00554BFFE6FBB7BAE39D,
+	UnityDebug_Awake_mE0AE29D996332EFAC4CF2C946488844FC417D6A6,
+	UnityDebug_OnApplicationPause_m28F0154E2072830E24687E1CFF27584F2E91B32E,
+	UnityDebug__ctor_m2A42FAD94D11E0896E165AE3D8B0BC7B61AE77F7,
+};
+static const int32_t s_InvokerIndices[28] = 
+{
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	2884,
+	2884,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	3569,
+	2884,
+	3569,
+	3569,
+	3569,
+	2818,
+	3569,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	28,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
