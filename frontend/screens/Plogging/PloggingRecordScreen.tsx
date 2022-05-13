65726754adb42ef4d98c67439fdd3d69dc527213
Hui-Story <leejaehee0728@gmail.com>
2022-05-14 00:26:33 +0900

    feat: 플로깅 사진 외부 SNS 공유
    플로깅 결과/기록 사진 외부 SNS 공유 연동
    'react-native-share' 라이브러리 설치
    
    
    Former-commit-id: b7ab83489fe4030cd08af261beaa5acd595df5ef

--- frontend/screens/Plogging/PloggingRecordScreen.tsx
+++ frontend/screens/Plogging/PloggingRecordScreen.tsx
@@ -1,4 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+// Hooks
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {useQuery} from 'react-query';
+// Api & Types
+import {getPloggingDetail} from '../../api/plogging';
+import {RootStackNavigationProp, RootStackParamList} from '../types';
+// Components
 import {
   Text,
   View,
@@ -7,17 +14,27 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
-import {RootStackNavigationProp, RootStackParamList} from '../types';
-import {useQuery} from 'react-query';
-import {getPloggingDetail} from '../../api/plogging';
+import {ActivityIndicator, Colors} from 'react-native-paper';
+import ViewShot from 'react-native-view-shot';
+import Share from 'react-native-share';
 
 const styles = (marginL?: any, marginR?: any, justify?: any, align?: any) =>
   StyleSheet.create({
+    loadingContainer: {
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
     container: {
       padding: 20,
       justifyContent: justify || 'flex-start',
       alignItems: align || 'flex-start',
+      backgroundColor: '#ffffff',
+    },
+    scrollContainer: {
+      backgroundColor: '#ffffff',
+      paddingBottom: 10,
     },
     img: {
       width: 300,
@@ -45,6 +62,11 @@ const fontStyles = (size?: any, weight?: any, color?: any, align?: any) =>
       color: color || '#000',
       textAlign: align || 'auto',
     },
+    loadingText: {
+      fontSize: 20,
+      color: Colors.blueA100,
+      marginTop: 10,
+    },
   });
 
 type PloggingRecordScreenRouteProp = RouteProp<
@@ -65,6 +87,7 @@ function PloggingRecordScreen() {
     min: '00',
     sec: '00',
   });
+  const viewShot = useRef();
 
   useEffect(() => {
     if (ploggingData) {
@@ -80,45 +103,69 @@ function PloggingRecordScreen() {
     }
   }, [ploggingData]);
 
+  const onShare = async () => {
+    try {
+      const uri = await getPhotoUri();
+      const options = {
+        title: 'title',
+        message: 'message',
+        url: uri,
+        type: 'image/jpeg',
+      };
+      await Share.open(options);
+    } catch (error) {}
+  };
+
+  const getPhotoUri = async (): Promise<string> => {
+    const uri = await viewShot.current?.capture();
+    return uri;
+  };
+
   if (!ploggingData || isLoading) {
     return (
-      <View>
-        <Text>로딩중</Text>
+      <View style={styles().loadingContainer}>
+        <ActivityIndicator animating={true} size={48} color={Colors.blueA100} />
+        <Text style={fontStyles().loadingText}>기록을 불러오는 중</Text>
       </View>
     );
   }
 
   return (
     <View>
-      <View style={styles().container}>
-        <Text
-          style={
-            fontStyles(30, '600', null).recordText
-          }>{`${ploggingDate[0]}년 ${ploggingDate[1]}월 ${ploggingDate[2]}일`}</Text>
-        <Text style={fontStyles(20, '600', null).recordText}>총 거리</Text>
-        <Text style={fontStyles(24, '600', null).recordText}>
-          {ploggingData.distance} km
-        </Text>
-        <Text style={fontStyles(18, '600', null).recordText}>칼로리</Text>
-        <Text style={fontStyles(20, '600', null).recordText}>
-          {ploggingData.calories} kcal
-        </Text>
-        <Text style={fontStyles(18, '600', null).recordText}>경과 시간</Text>
-        <Text style={fontStyles(20, '600', null).recordText}>
-          {`${ploggingTime.hour}:${ploggingTime.min}:${ploggingTime.sec}`}
-        </Text>
-      </View>
-      <ScrollView horizontal={true}>
-        <Image source={{uri: ploggingData.result_img}} style={styles(40).img} />
-        <Image
-          source={{
-            uri: ploggingData.route_img,
-          }}
-          style={styles(30, 40).img}
-        />
-      </ScrollView>
+      <ViewShot ref={viewShot} options={{format: 'jpg', quality: 0.9}}>
+        <View style={styles().container}>
+          <Text
+            style={
+              fontStyles(30, '600', null).recordText
+            }>{`${ploggingDate[0]}년 ${ploggingDate[1]}월 ${ploggingDate[2]}일`}</Text>
+          <Text style={fontStyles(20, '600', null).recordText}>총 거리</Text>
+          <Text style={fontStyles(24, '600', null).recordText}>
+            {ploggingData.distance} km
+          </Text>
+          <Text style={fontStyles(18, '600', null).recordText}>칼로리</Text>
+          <Text style={fontStyles(20, '600', null).recordText}>
+            {ploggingData.calories} kcal
+          </Text>
+          <Text style={fontStyles(18, '600', null).recordText}>경과 시간</Text>
+          <Text style={fontStyles(20, '600', null).recordText}>
+            {`${ploggingTime.hour}:${ploggingTime.min}:${ploggingTime.sec}`}
+          </Text>
+        </View>
+        <ScrollView style={styles().scrollContainer} horizontal={true}>
+          <Image
+            source={{uri: ploggingData.result_img}}
+            style={styles(40).img}
+          />
+          <Image
+            source={{
+              uri: ploggingData.route_img,
+            }}
+            style={styles(30, 40).img}
+          />
+        </ScrollView>
+      </ViewShot>
       <View style={styles(null, null, 'center', 'center').container}>
-        <TouchableOpacity onPress={() => {}} style={styles().btn}>
+        <TouchableOpacity onPress={() => onShare()} style={styles().btn}>
           <Text style={fontStyles(20, '600', null).recordText}>공유하기</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.pop()} style={styles().btn}>
