33c22431b330d39a67fb15b8b7494ee443c1a42d
Hui-Story <leejaehee0728@gmail.com>
2022-05-17 16:28:21 +0900

    feat: 하단 탭 아이콘, 타이틀 수정
    Former-commit-id: cfdd7827a0abafc1f771c81c5b06c161f3941371

--- frontend/screens/MainScreen.tsx
+++ frontend/screens/MainScreen.tsx
@@ -6,6 +6,9 @@ import UserScreen from './UserScreen';
 import CommunityScreen from './CommunityScreen';
 import SurveyScreen from './SurveyScreen';
 import AvatarScreen from './AvatarScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconI from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator<MainTabParamList>();
 
@@ -13,15 +16,28 @@ function MainScreen() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: '#fb8c00',
+        tabBarActiveTintColor: '#5FA2E5',
         headerShown: false,
       }}>
-      <Tab.Screen name="Plogging" component={PloggingScreen} />
+      <Tab.Screen
+        name="Plogging"
+        component={PloggingScreen}
+        options={{
+          title: '플로깅',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="directions-run" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="User"
         component={UserScreen}
         options={{
           unmountOnBlur: true,
+          title: '내 정보',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="person" size={size} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -29,14 +45,31 @@ function MainScreen() {
         component={CommunityScreen}
         options={{
           unmountOnBlur: true,
+          title: '커뮤니티',
+          tabBarIcon: ({color, size}) => (
+            <IconI name="chatbubbles-sharp" size={size} color={color} />
+          ),
         }}
       />
-      <Tab.Screen name="Survey" component={SurveyScreen} />
+      <Tab.Screen
+        name="Survey"
+        component={SurveyScreen}
+        options={{
+          title: '설문조사',
+          tabBarIcon: ({color, size}) => (
+            <IconM name="clipboard-check-multiple" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Avatar"
         component={AvatarScreen}
         options={{
           unmountOnBlur: true,
+          title: '마이룸',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="face-retouching-natural" size={size} color={color} />
+          ),
         }}
       />
     </Tab.Navigator>
