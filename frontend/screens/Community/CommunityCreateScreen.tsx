19d04ca3ff080d8dd4cd40f6f3e3b38b9ed39724
snowwhitewolf <whdgus1029@naver.com>
2022-05-18 23:24:47 +0900

    feat : 커뮤니티 생성 수정 변경
    Former-commit-id: cdcfb496e004e564dda08cc1c6cb47f4843b7e93

--- frontend/screens/Community/CommunityCreateScreen.tsx
+++ frontend/screens/Community/CommunityCreateScreen.tsx
@@ -6,6 +6,7 @@ import {
   TextInput,
   Alert,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import {useMutation, useQueryClient} from 'react-query';
 import {createCommunity} from '../../api/community';
@@ -18,8 +19,91 @@ import {
 import {useNavigation} from '@react-navigation/native';
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#ffffff',
+  },
+  menuTitle: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: '800',
+    alignSelf: 'center',
+  },
+  formContainer: {
+    height: '60%',
+  },
+  imageEditMask: {
+    position: 'absolute',
+    top: '70%',
+    width: '100%',
+    height: '30%',
+    backgroundColor: '#000000',
+    opacity: 0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitButton: {
+    width: '100%',
+    backgroundColor: 'rgba(95, 162, 229, 0.8)',
+    marginTop: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    alignSelf: 'center',
+  },
+  submitText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '800',
+    letterSpacing: 2,
+  },
+  titleContainer: {
+    width: '100%',
+    backgroundColor: 'rgba(95, 162, 229, 0.8)',
+    height: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    height: '100%',
+  },
+  imageEdit: {
+    height: '20%',
+  },
+  nameInput: {
+    width: '100%',
+    height: '20%',
+    borderBottomWidth: 1,
+    borderColor: '#c4c4c4',
+    padding: 10,
+  },
+  tagInput: {
+    flexDirection: 'row',
+    width: '100%',
+    height: '20%',
+    borderBottomWidth: 1,
+    borderColor: '#c4c4c4',
+  },
+  desInput: {
+    width: '100%',
+    height: '60%',
+    padding: 10,
+  },
+  label: {
+    color: '#c4c4c4',
+  },
+  textInput: {
+    color: '#000000',
+  },
+  locaContaier: {
+    width: '50%',
+    borderRightWidth: 1,
+    borderColor: '#c4c4c4',
+    padding: 10,
+  },
+  tagContaier: {
+    width: '50%',
+    padding: 10,
   },
 });
 
@@ -33,13 +117,13 @@ function CommunityCreateScreen() {
     tag: '',
     title: '',
   });
-  const [uri, setUri] = React.useState<string>('https://ecolog-bucket.s3.ap-northeast-2.amazonaws.com/Ecolog_file_default_profile.jpg');
+  const [uri, setUri] = React.useState<string>('Image');
   const {mutate: createCommu} = useMutation(createCommunity, {
     onSuccess: data => {
-      navigation.pop();
-      navigation.navigate('CommunityHome', {id: data.no});
       queryClient.invalidateQueries('CommunityList');
       queryClient.invalidateQueries('myCommunity');
+      navigation.pop();
+      navigation.navigate('CommunityHome', {id: data.no});
     },
   });
   const submitCreate = () => {
@@ -52,7 +136,6 @@ function CommunityCreateScreen() {
       communityImgData: communityImgData,
       communityInfo: communityInfo,
     });
-    Alert.alert('생성이 완료되었습니다.');
   };
   const imagePickerOption: CameraOptions = {
     mediaType: 'photo',
@@ -69,57 +152,77 @@ function CommunityCreateScreen() {
   };
   return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.menuTitle}>커뮤니티 만들기</Text>
+      </View>
       <TouchableOpacity
-        onPress={() => launchImageLibrary(imagePickerOption, onPickImage)}>
-        <Text>이미지</Text>
+        onPress={() => launchImageLibrary(imagePickerOption, onPickImage)}
+        style={styles.imageEdit}>
+        <Image
+          style={styles.img}
+          resizeMode="cover"
+          source={{
+            uri: uri,
+          }}
+        />
+        <View style={styles.imageEditMask}>
+          <Text style={{fontSize: 16, color: '#ffffff'}}>편집</Text>
+        </View>
       </TouchableOpacity>
-      <Text>이름</Text>
-      <TextInput
-        placeholder="이름"
-        value={communityInfo.title}
-        onChangeText={(text: string) =>
-          setCommunityInfo({...communityInfo, title: text})
-        }
-        returnKeyType="done"
-      />
-      <Text>설명글</Text>
-      <TextInput
-        placeholder="설명"
-        value={communityInfo.description}
-        onChangeText={(text: string) =>
-          setCommunityInfo({...communityInfo, description: text})
-        }
-        returnKeyType="done"
-      />
-      <Text>시도</Text>
-      <TextInput
-        placeholder="시도"
-        value={communityInfo.sido}
-        onChangeText={(text: string) =>
-          setCommunityInfo({...communityInfo, sido: text})
-        }
-        returnKeyType="done"
-      />
-      <Text>시군구</Text>
-      <TextInput
-        placeholder="시군구"
-        value={communityInfo.sigungu}
-        onChangeText={(text: string) =>
-          setCommunityInfo({...communityInfo, sigungu: text})
-        }
-        returnKeyType="done"
-      />
-      <Text>태그</Text>
-      <TextInput
-        placeholder="태그"
-        value={communityInfo.tag}
-        onChangeText={(text: string) =>
-          setCommunityInfo({...communityInfo, tag: text})
-        }
-        returnKeyType="done"
-      />
-      <TouchableOpacity onPress={() => submitCreate()}>
-        <Text>생성하기</Text>
+      <View style={styles.formContainer}>
+        <View style={styles.nameInput}>
+          <Text style={styles.label}>이름</Text>
+          <TextInput
+            placeholder="이름"
+            value={communityInfo.title}
+            onChangeText={(text: string) =>
+              setCommunityInfo({...communityInfo, title: text})
+            }
+            returnKeyType="done"
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.tagInput}>
+          <View style={styles.locaContaier}>
+            <Text style={styles.label}>지역</Text>
+            <TextInput
+              placeholder="지역"
+              value={communityInfo.sido}
+              onChangeText={(text: string) =>
+                setCommunityInfo({...communityInfo, sido: text})
+              }
+              returnKeyType="done"
+            />
+          </View>
+          <View style={styles.tagContaier}>
+            <Text style={styles.label}>태그</Text>
+            <TextInput
+              placeholder="태그"
+              value={communityInfo.tag}
+              onChangeText={(text: string) =>
+                setCommunityInfo({...communityInfo, tag: text})
+              }
+              returnKeyType="done"
+            />
+          </View>
+        </View>
+        <View style={styles.desInput}>
+          <Text style={styles.label}>설명</Text>
+          <TextInput
+            placeholder="설명"
+            value={communityInfo.description}
+            onChangeText={(text: string) =>
+              setCommunityInfo({...communityInfo, description: text})
+            }
+            returnKeyType="done"
+            style={styles.textInput}
+          />
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.submitButton}
+        onPress={() => submitCreate()}>
+        <Text style={styles.submitText}>생성하기</Text>
       </TouchableOpacity>
     </View>
   );
