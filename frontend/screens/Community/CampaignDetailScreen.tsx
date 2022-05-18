13caea5b8a5fad54efdc050f5e4b2c6722653e71
snowwhitewolf <whdgus1029@naver.com>
2022-05-18 11:55:46 +0900

    feat : 캠페인 디테일 수정
    Former-commit-id: 2579402bd33c268f2e02ac2c8903f5bb89312d9e

--- frontend/screens/Community/CampaignDetailScreen.tsx
+++ frontend/screens/Community/CampaignDetailScreen.tsx
@@ -16,7 +16,7 @@ import {
 import {useNavigation} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../modules';
-import {useMutation} from 'react-query';
+import {useMutation, useQueryClient} from 'react-query';
 const styles = StyleSheet.create({
   container: {
     width: '100%',
@@ -31,14 +31,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   button: {
-    backgroundColor: 'rgb(139, 139, 139)',
-    width: '20%',
+    backgroundColor: 'rgb(127, 111, 255)',
+    marginTop: 'auto',
+    width: '100%',
+    borderRadius: 10,
     height: '10%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    marginBottom: '20%',
     fontSize: 20,
     fontWeight: '800',
     color: '#ffffff',
@@ -53,7 +54,9 @@ const styles = StyleSheet.create({
     height: '100%',
     flex: 2,
     borderRadius: 10,
-    backgroundColor: 'rgb(139, 139, 139)',
+    backgroundColor: 'rgb(121, 190, 36)',
+    justifyContent: 'flex-end',
+    padding: 20,
     marginTop: 10,
     marginBottom: 10,
     marginRight: 10,
@@ -63,21 +66,22 @@ const styles = StyleSheet.create({
     flex: 1,
     borderRadius: 10,
     marginTop: 10,
+    padding: 20,
     marginBottom: 10,
-    backgroundColor: 'rgb(139, 139, 139)',
+    backgroundColor: 'rgb(255, 122, 79)',
+    justifyContent: 'flex-end',
   },
   desContainer: {
     height: '15%',
     width: '100%',
-    backgroundColor: 'rgb(139, 139, 139)',
+    backgroundColor: 'rgb(62, 68, 75)',
+    padding: 20,
     borderRadius: 10,
     marginTop: 10,
+    marginBottom: 10,
   },
   mainTitle: {
     fontSize: 20,
-    margin: 30,
-    marginLeft: 0,
-    marginBottom: 0,
     alignSelf: 'flex-start',
     color: '#000000',
   },
@@ -89,17 +93,51 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     color: '#636363',
   },
+  firstFont: {
+    fontSize: 16,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  contentFont: {
+    fontSize: 14,
+    color: '#ffffff',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#ffffff',
+    fontWeight: '600',
+    letterSpacing: 2,
+  },
+  editButton: {
+    borderWidth: 1,
+    borderColor: '#979797',
+    marginLeft: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+  },
 });
 
 function CampaignDetailScreen({route}: any) {
-  const {mutate: campaignJoin} = useMutation(postCampaignJoin);
+  const queryClient = useQueryClient();
+  const {mutate: campaignJoin} = useMutation(postCampaignJoin, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('campaignDetail');
+    },
+  });
   const Join = () => {
     campaignJoin({communityNo: route.params.no, campaignNo: route.params.id});
-    Alert.alert('가입이 완료되었습니다.');
   };
   const myInfo = useSelector((state: RootState) => state.user.user);
   const navigation = useNavigation<any>();
-  const {mutate: campaignDelete} = useMutation(deleteCampaign);
+  const {mutate: campaignDelete} = useMutation(deleteCampaign, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('campaignList');
+      navigation.pop();
+    },
+  });
   const {data: data, isLoading} = useQuery(
     ['campaignDetail', {no: route.params.no, id: route.params.id}],
     () => getCampaignDetail(route.params.no, route.params.id),
@@ -111,6 +149,7 @@ function CampaignDetailScreen({route}: any) {
       </View>
     );
   }
+  const nowPeople = data.join_personnel.length;
   const deleteCam = () => {
     campaignDelete({
       communitySeq: route.params.no,
@@ -120,43 +159,54 @@ function CampaignDetailScreen({route}: any) {
   };
   return (
     <View style={styles.container}>
-      <Text style={styles.mainTitle}>{data.title}</Text>
-      <Text style={styles.date}>
-        {data.start_date} ~ {data.end_date}
-      </Text>
+      <View style={{flexDirection: 'row', width: '100%'}}>
+        <View style={{marginRight: 'auto'}}>
+          <Text style={styles.mainTitle}>{data.title}</Text>
+          <Text style={styles.date}>
+            {data.start_date} ~ {data.end_date}
+          </Text>
+        </View>
+        {data.writer.email === myInfo.data?.email && (
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() =>
+              navigation.navigate('CampaignEdit', {
+                data: data,
+                no: route.params.no,
+              })
+            }>
+            {/* 연필 아이콘 */}
+            <Text>수정</Text>
+          </TouchableOpacity>
+        )}
+        {data.writer.email === myInfo.data?.email && (
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() => deleteCam()}>
+            <Text>삭제</Text>
+          </TouchableOpacity>
+        )}
+      </View>
       <Image source={{uri: data.image}} style={styles.img} />
       <View style={styles.contentContainer}>
         <View style={styles.locationContainer}>
-          <Text>{data.location}</Text>
+          <Text style={styles.firstFont}>장소</Text>
+          <Text style={styles.firstFont}>{data.location}</Text>
         </View>
         <View style={styles.countContainer}>
-          <Text>{data.max_personnel}</Text>
+          <Text style={styles.firstFont}>인원</Text>
+          <Text style={styles.firstFont}>
+            {nowPeople} / {data.max_personnel}
+          </Text>
         </View>
       </View>
       <View style={styles.desContainer}>
-        <Text>{data.content}</Text>
+        <Text style={styles.contentFont}>{data.content}</Text>
       </View>
       <TouchableOpacity onPress={() => Join()} style={styles.button}>
-        <Text>참가하기</Text>
+        {data.joining && <Text style={styles.buttonText}>참가 취소하기</Text>}
+        {!data.joining && <Text style={styles.buttonText}>참가 하기</Text>}
       </TouchableOpacity>
-
-      {data.writer.email === myInfo.data?.email && (
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate('CampaignEdit', {
-              data: data,
-              no: route.params.no,
-            })
-          }>
-          {/* 연필 아이콘 */}
-          <Text>수정</Text>
-        </TouchableOpacity>
-      )}
-      {data.writer.email === myInfo.data?.email && (
-        <TouchableOpacity onPress={() => deleteCam()}>
-          <Text>삭제</Text>
-        </TouchableOpacity>
-      )}
     </View>
   );
 }
