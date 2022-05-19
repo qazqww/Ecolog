5f5d41a95e2f1f41d9bf38f24e328c0f6db3367a
Hui-Story <leejaehee0728@gmail.com>
2022-05-19 12:56:24 +0900

    feat: console.log 삭제
    Former-commit-id: 09eb22edd33774525622fb43a1bb2d73ab9b20e3

--- frontend/api/community.ts
+++ frontend/api/community.ts
@@ -98,7 +98,6 @@ export async function createPost(postData: CreatePostData) {
     },
     transformRequest: formData => formData,
   });
-  console.log(response.data);
   return response.data;
 }
 
