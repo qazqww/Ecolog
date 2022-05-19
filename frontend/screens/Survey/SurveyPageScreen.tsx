fb459311fca84f0423857512a65673a02e2fc009
ek_13 <kldoggod@gmail.com>
2022-05-19 21:01:54 +0900

    design : 에코지수 테스트 이미지 수정
    Former-commit-id: 6234b5e76a4c40dd4dd369b47eaeba732674c45a

--- frontend/screens/Survey/SurveyPageScreen.tsx
+++ frontend/screens/Survey/SurveyPageScreen.tsx
@@ -25,7 +25,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '그냥 버린다',
       s1: 10,
       s2: 0,
-      img: 'http://www.newstnt.com/news/photo/202012/68814_63130_4151.jpg',
+      img: require('../../assets/survey/q1.jpg'),
     },
     {
       q: '2. 나무젓가락을 잘못 뜯은 상황에서 당신의 행동은?',
@@ -33,7 +33,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '그냥 쓴다',
       s1: 0,
       s2: 10,
-      img: 'https://mblogthumb-phinf.pstatic.net/MjAxODEyMjJfMjE2/MDAxNTQ1NDc0OTA2NTA4.Xis7eo5XMUqaPQ6qlvFIciRxlm8tD_GncgBqgrrYzs8g.FRrEDGXwP4YKPlEzcyou_PnBqpTUl8KIqxEyrZZsWasg.JPEG.oksp96/IMG_9788.jpg?type=w800',
+      img: require('../../assets/survey/q2.jpg'),
     },
     {
       q: '3. 음식물이 묻은 플라스틱 용기를 버릴 때 당신의 행동은?',
@@ -41,7 +41,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '그냥 버린다',
       s1: 10,
       s2: 0,
-      img: 'https://image.kmib.co.kr/online_image/2020/1003/611718110015062582_8.jpg',
+      img: require('../../assets/survey/q3.jpg'),
     },
     {
       q: '4. 음식 배달 주문 시 당신의 행동은?',
@@ -49,7 +49,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '집에 있는 수저를 사용한다',
       s1: 0,
       s2: 10,
-      img: 'https://cdn.4th.kr/news/photo/202201/2012450_75533_1912.jpg',
+      img: require('../../assets/survey/q4.jpg'),
     },
     {
       q: '5. 쓰레기를 버려야하는데 주변에 쓰레기통이 없다. 당신의 행동은?',
@@ -57,7 +57,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '그냥 길에 버린다',
       s1: 10,
       s2: 0,
-      img: 'https://gnews.gg.go.kr/OP_UPDATA/UP_DATA/_FILEZ/202009/20200928053913011397081.jpg',
+      img: require('../../assets/survey/q5.jpg'),
     },
     {
       q: '6. 계란 껍데기는 음식물 쓰레기이다.',
@@ -65,15 +65,15 @@ function SurveyPageScreen({navigation}: any) {
       a2: '맞다',
       s1: 10,
       s2: 0,
-      img: 'https://t3.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/Jp6/image/rf9Q87WLTLndAXbnGKimylxD9Rs.jpg',
+      img: require('../../assets/survey/q6.jpg'),
     },
     {
       q: '7. 길을 가다 바닥에 버려진 쓰레기를 발견했다. 당신의 행동은?',
       a1: '주워서 쓰레기통에 버린다',
-      a2: '나도 동참한다',
+      a2: '나도 길에 버린다',
       s1: 10,
       s2: 0,
-      img: 'http://newsimg.hankookilbo.com/2016/08/18/201608180477237489_1.jpg',
+      img: require('../../assets/survey/q7.jpg'),
     },
     {
       q: '8. 사용하고 남은 고추장을 버리려고 한다. 당신의 선택은?',
@@ -81,7 +81,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '아니다 일반쓰레기다!',
       s1: 0,
       s2: 10,
-      img: 'https://dafarm.net/data/goods/55/2021/02/190_temp_16127648370430view.jpg',
+      img: require('../../assets/survey/q8.jpg'),
     },
     {
       q: '9. 폐형광등을 버리려고 한다. 당신의 행동은?',
@@ -89,7 +89,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '폐형광등 수거함에 버린다',
       s1: 0,
       s2: 10,
-      img: 'https://t1.daumcdn.net/cfile/tistory/994D2B335ABF98C01B',
+      img: require('../../assets/survey/q9.jpg'),
     },
     {
       q: '10. 아래 유리병을 분리수거하려고 한다. 당신의 행동은?',
@@ -97,7 +97,7 @@ function SurveyPageScreen({navigation}: any) {
       a2: '그냥 뚜껑이랑 같이 버린다',
       s1: 0,
       s2: 10,
-      img: 'https://blisgo.com/wp-content/uploads/2020/07/%EC%9C%A0%EB%A6%AC%EB%B3%91.jpg',
+      img: require('../../assets/survey/q10.jpg'),
     },
   ];
 
@@ -161,10 +161,11 @@ function SurveyPageScreen({navigation}: any) {
       marginRight: 'auto',
     },
     img: {
-      width: '75%',
-      aspectRatio: 1,
+      width: 300,
+      height: 300,
       marginTop: '10%',
       marginBottom: '5%',
+      borderRadius: 5,
     },
     dialogContainer: {
       height: 200,
@@ -209,12 +210,7 @@ function SurveyPageScreen({navigation}: any) {
           <Icon name="arrow-back-ios" size={28} color="#4D4D4D" />
         </TouchableHighlight>
         <Text style={styles.question}>{survey[step].q}</Text>
-        <Image
-          style={styles.img}
-          source={{
-            uri: survey[step].img,
-          }}
-        />
+        <Image style={styles.img} source={survey[step].img} />
         <Button
           mode="contained"
           style={styles.button}
